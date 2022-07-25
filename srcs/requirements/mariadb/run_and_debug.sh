837bbf3bf9bba2a2f395f56853b7330ea8354a3c
RobertoRobrodri <karmmero@gmail.com>
2022-07-25 22:50:31 +0200

    working on volumes

--- srcs/requirements/mariadb/run_and_debug.sh
+++ srcs/requirements/mariadb/run_and_debug.sh
@@ -2,5 +2,5 @@
 #Use for debbuging single container
 
 docker build -t $1 .
-docker run --name $1 -d -p 80:80 -p 443:443 $1
+docker run --name $1 -d -p 80:80 -e MYSQL_ROOT_PASSWORD=paco $1
 docker exec -it $1 bash
---
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+service mysql start
+
+mysql -e "CREATE USER '$MYSQL_ADMIN_USER' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
+mysql -e "CREATE USER '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD';"
+
+#mysql -e "REVOKE ALL PRIVILEGES FROM '$MYSQL_USER';"
+
+while true; do sleep 1d; done
