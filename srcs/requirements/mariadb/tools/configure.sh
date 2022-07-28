71c5d4feaed97bab70289f36a370bd9de55a6b14
RobertoRobrodri <karmmero@gmail.com>
2022-07-28 12:14:04 +0200

    Found the fault creating the users

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -2,11 +2,21 @@
 
 service mysql start
 
+if [[ $(mysql -e "SELECT user FROM mysql.user WHERE user = '${MYSQL_ADMIN_USER}';") == "user" ]]
+
+then
+
 mysql -e "CREATE USER '${MYSQL_ADMIN_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+fi
+
+if [[ $(mysql -e "SELECT user FROM mysql.user WHERE user = '${MYSQL_USER}';") == "user" ]]
+
+then
 
 mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+fi
 
 #mysql -e "REVOKE ALL PRIVILEGES FROM '${MYSQL_USER}';"
 
