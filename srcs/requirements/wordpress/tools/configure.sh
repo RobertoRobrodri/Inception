49061d7c99e674829e7b4fd755c108dcbc34285a
RobertoRobrodri <karmmero@gmail.com>
2022-08-02 11:41:20 +0200

    updates wp environment

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+echo Updating environment
 
-if [ -d "/var/www/html" ]
+sed -i "s/database_name_here/$DB_NAME/g" /tmp/wordpress/wp-config-sample.php
+sed -i "s/username_here/$MYSQL_USER/g" /tmp/wordpress/wp-config-sample.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" /tmp/wordpress/wp-config-sample.php
+cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
+rm -rf tmp/wordpress/wp-config-sample.php
+
+if [ "ls -A /var/www/html" ]
 then
-	echo "Direcotry already exists"
+	echo "Directory not empty"
 else
 	mv tmp/wordpress /var/www/html
 fi
--- srcs/requirements/wordpress/tools/env_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i "s/Database_name/$DB_NAME/g" /usr/share/webapps/wordpress/wp-config-sample.php
-sed -i "s/Database_user/$MYSQL_USER/g" /usr/share/webapps/wordpress/wp-config-sample.php
-sed -i "s/Database_password/$MYSQL_PASSWORD/g" /usr/share/webapps/wordpress/wp-config-sample.php
