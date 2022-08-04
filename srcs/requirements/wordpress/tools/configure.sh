8640671f3dc2bf3cf9b90747304050faccbe8993
RobertoRobrodri <karmmero@gmail.com>
2022-08-04 15:35:43 +0200

    pls upload

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -5,7 +5,8 @@ sed -i "s/database_name_here/$DB_NAME/g" /tmp/wordpress/wp-config-sample.php
 sed -i "s/username_here/$MYSQL_USER/g" /tmp/wordpress/wp-config-sample.php
 sed -i "s/password_here/$MYSQL_PASSWORD/g" /tmp/wordpress/wp-config-sample.php
 cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
-rm -rf tmp/wordpress/wp-config-sample.php
+#rm -rf tmp/wordpress/wp-config-sample.php
 
-mv tmp/wordpress /var/www/html
+#Deberias cambiar esto y comprobar el directorio
+mv -f tmp/wordpress /var/www/html
 while true; do sleep 1d; done
