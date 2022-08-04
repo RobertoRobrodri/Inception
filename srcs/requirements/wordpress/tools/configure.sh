baf72307e9db8b8fa7ae51c2ebd608588c93a544
Anaximandro-Robrodri <karmmero@gmail.com>
2022-08-04 22:16:31 +0200

    FPM conf

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -4,9 +4,11 @@ echo Updating environment
 sed -i "s/database_name_here/$DB_NAME/g" /tmp/wordpress/wp-config-sample.php
 sed -i "s/username_here/$MYSQL_USER/g" /tmp/wordpress/wp-config-sample.php
 sed -i "s/password_here/$MYSQL_PASSWORD/g" /tmp/wordpress/wp-config-sample.php
+sed -i "s/localhost/mariadb/g" /tmp/wordpress/wp-config-sample.php
 cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
-#rm -rf tmp/wordpress/wp-config-sample.php
+rm -rf tmp/wordpress/wp-config-sample.php
 
 #Deberias cambiar esto y comprobar el directorio
 mv -f tmp/wordpress /var/www/html
+#exec php-fpm7.3 --nodaemonize
 while true; do sleep 1d; done
