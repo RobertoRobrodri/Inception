bf5205b20ea056836931efc32c55c0225fe79e99
RobertoRobrodri <karmmero@gmail.com>
2022-08-05 09:29:46 +0200

    nginx and wordpress connection working

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo Updating environment
-
 sed -i "s/database_name_here/$DB_NAME/g" /tmp/wordpress/wp-config-sample.php
 sed -i "s/username_here/$MYSQL_USER/g" /tmp/wordpress/wp-config-sample.php
 sed -i "s/password_here/$MYSQL_PASSWORD/g" /tmp/wordpress/wp-config-sample.php
@@ -10,5 +8,4 @@ rm -rf tmp/wordpress/wp-config-sample.php
 
 #Deberias cambiar esto y comprobar el directorio
 mv -f tmp/wordpress /var/www/html
-#exec php-fpm7.3 --nodaemonize
-while true; do sleep 1d; done
+exec php-fpm7.3 --nodaemonize
