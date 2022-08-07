478bccc5a86e22b59be89c506c80d079337dc9fa
RobertoRobrodri <karmmero@gmail.com>
2022-08-07 16:13:26 +0200

    No more shady tricks

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
+#/etc/init.d/mysql start
+
 service mysql start
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
-if [[ $(mysql -e "SELECT user FROM mysql.user WHERE user = '${MYSQL_ADMIN_USER}';") == "user" ]]
-
-then
-
-mysql -e "CREATE USER '${MYSQL_ADMIN_USER}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-fi
 
-if [[ $(mysql -e "SELECT user FROM mysql.user WHERE user = '${MYSQL_USER}';") == "user" ]]
-
-then
-
-mysql -e "CREATE USER '${MYSQL_USER}' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-fi
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
 mysql -e "FLUSH PRIVILEGES"
 #mysql -e "REVOKE ALL PRIVILEGES FROM '${MYSQL_USER}';"
-while true; do sleep 1d; done
