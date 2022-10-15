985f56b967506571fd26000c572db5ddd4a346d5
root <karmmero@gmail.com>
2022-10-15 16:15:32 +0200

    New user

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -11,5 +11,6 @@ then
 	sed -i "s/localhost/mariadb/g" wp-config.php
 	echo ready to install
 	wp core install --url=robrodri.42.fr --title=Inception --admin_user=$MYSQL_ADMIN_USER --admin_password=$MYSQL_PASSWORD --admin_email=info@example.com --allow-root
+	wp user create $MYSQL_USER useremail@example.com --role=subscriber --user_pass=$MYSQL_PASSWORD --allow-root
 fi
 exec php-fpm7.3 --nodaemonize
